0dfbaec2a840d5e68627f4399460bce1c27a76d0
daggron <abhay.sharma@smallcase.com>
2025-02-16 19:18:58 +0530

    fix: add utils to mesaure time

---
+++ src/utils/timeMesaure.ts
@@ -0,0 +1,7 @@
+export function measureExecutionTime(taskName: string, fn: () => void) {
+  const start = process.hrtime();
+  fn();
+  const [seconds, nanoseconds] = process.hrtime(start);
+  const timeMs = (seconds * 1000 + nanoseconds / 1e6).toFixed(2);
+  console.log(`🕒 ${taskName} completed in ${timeMs}ms`);
+}
